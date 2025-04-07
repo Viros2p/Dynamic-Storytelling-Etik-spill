d872f631040546544948a3108d9d2dfa74e71689
Viros2p <arma0096x@gmail.com>
2025-04-07 15:37:53 +0200

    Har lavet mor far og søsteren kan gå.
    Moren og Faren kan ingen ting. Søsteren kan gå, men har ingen animations

--- Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
+++ Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
@@ -2,45 +2,71 @@ using UnityEngine;
 
 public class RandomWalkinNPC : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float range;
-    [SerializeField] float MaxDistance;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float MaxDistance;
 
     Vector2 WayPoint;
-    Animator animator; // Animator reference
 
+    //NPC Flipping 
+    //public float moveSpeed = 2f;  // Hastighed for NPC's bevægelse
+    private Vector3 direction;
+    private Animator animator;  // Reference til animatoren
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetNewDestination();
-        animator = GetComponent<Animator>(); // Getting Animator component
+
+
+        // Få fat i animatoren
+        animator = GetComponent<Animator>();
+
+        // Start bevægelsen
+        direction = Vector3.right;  // NPC'en skal bevæge sig mod højre
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Move NPC towards the waypoint
         transform.position = Vector2.MoveTowards(transform.position, WayPoint, speed * Time.deltaTime);
-
-        // Check if NPC has reached the waypoint
         if (Vector2.Distance(transform.position, WayPoint) < range)
         {
             SetNewDestination();
         }
 
-        // Determine direction for animation flip
-        if (transform.position.x < WayPoint.x)
+        // Bevæg NPC'en mod højre (på X-aksen)
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // Tjek om NPC'en bevæger sig mod højre
+        if (direction == Vector3.right && transform.localScale.x < 0)
         {
-            // NPC moving right
-            animator.SetBool("MovingRight", true);
+            // Flip animationen
+            Flip();
         }
-        else
+        // Tjek om NPC'en bevæger sig mod venstre
+        else if (direction == Vector3.left && transform.localScale.x > 0)
         {
-            // NPC moving left
-            animator.SetBool("MovingRight", false);
+            // Flip animationen
+            Flip();
         }
+
     }
 
     void SetNewDestination()
     {
         WayPoint = new Vector2(Random.Range(-MaxDistance, MaxDistance), Random.Range(-MaxDistance, MaxDistance));
+    }
+
+    void Flip()
+    {
+        // Flip X-aksen af NPC'ens localScale
+        Vector3 scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
     }
 }
