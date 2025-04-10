0b69194915c39cb8239e5dadd1ccef6d67d96f56
Viros2p <arma0096x@gmail.com>
2025-04-10 12:06:50 +0200

    Har lavet hund og er i gang med at fixe søsterens movement og flipping

--- Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
+++ Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
@@ -11,23 +11,12 @@ public class RandomWalkinNPC : MonoBehaviour
 
     Vector2 WayPoint;
 
-    //NPC Flipping 
-    //public float moveSpeed = 2f;  // Hastighed for NPC's bevægelse
-    private Vector3 direction;
-    private Animator animator;  // Reference til animatoren
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetNewDestination();
 
-
-        // Få fat i animatoren
-        animator = GetComponent<Animator>();
-
-        // Start bevægelsen
-        direction = Vector3.right;  // NPC'en skal bevæge sig mod højre
     }
 
     // Update is called once per frame
@@ -39,22 +28,6 @@ public class RandomWalkinNPC : MonoBehaviour
             SetNewDestination();
         }
 
-        // Bevæg NPC'en mod højre (på X-aksen)
-        transform.Translate(direction * speed * Time.deltaTime);
-
-        // Tjek om NPC'en bevæger sig mod højre
-        if (direction == Vector3.right && transform.localScale.x < 0)
-        {
-            // Flip animationen
-            Flip();
-        }
-        // Tjek om NPC'en bevæger sig mod venstre
-        else if (direction == Vector3.left && transform.localScale.x > 0)
-        {
-            // Flip animationen
-            Flip();
-        }
-
     }
 
     void SetNewDestination()
@@ -62,11 +35,4 @@ public class RandomWalkinNPC : MonoBehaviour
         WayPoint = new Vector2(Random.Range(-MaxDistance, MaxDistance), Random.Range(-MaxDistance, MaxDistance));
     }
 
-    void Flip()
-    {
-        // Flip X-aksen af NPC'ens localScale
-        Vector3 scale = transform.localScale;
-        scale.x = -scale.x;
-        transform.localScale = scale;
-    }
 }
