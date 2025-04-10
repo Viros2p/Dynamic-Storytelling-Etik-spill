dc532054364d42857ddac254aa71c8485605ddc2
Viros2p <arma0096x@gmail.com>
2025-04-10 14:00:32 +0200

    Har lavet så de ikka gå igennm vægge og noget med søsteren

--- Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
+++ Assets/_Game_/Player/Scipts/RandomWalkinNPC.cs
@@ -2,37 +2,53 @@ using UnityEngine;
 
 public class RandomWalkinNPC : MonoBehaviour
 {
-    [SerializeField]
-    float speed;
-    [SerializeField]
-    float range;
-    [SerializeField]
-    float MaxDistance;
+    [SerializeField] float speed;
+    [SerializeField] float range;
+    [SerializeField] float maxDistance;
+    [SerializeField] float stuckCheckDelay = 1f; // Hvor lang tid før vi tjekker for "stuck"
 
-    Vector2 WayPoint;
+    private Vector2 wayPoint;
+    private Vector2 lastPosition;
+    private float stuckTimer = 0f;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetNewDestination();
-
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, WayPoint, speed * Time.deltaTime);
-        if (Vector2.Distance(transform.position, WayPoint) < range)
+        // Bevæg mod waypoint
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+
+        // Skift retning hvis vi er tæt på målet
+        if (Vector2.Distance(transform.position, wayPoint) < range)
         {
             SetNewDestination();
         }
 
+        // Tjek for "stuck"
+        stuckTimer += Time.deltaTime;
+        if (stuckTimer >= stuckCheckDelay)
+        {
+            float distanceMoved = Vector2.Distance(transform.position, lastPosition);
+            if (distanceMoved < 0.05f) // Ikke flyttet sig nok = sidder fast
+            {
+                SetNewDestination(); // Vælg ny retning
+            }
+
+            lastPosition = transform.position;
+            stuckTimer = 0f;
+        }
     }
 
     void SetNewDestination()
     {
-        WayPoint = new Vector2(Random.Range(-MaxDistance, MaxDistance), Random.Range(-MaxDistance, MaxDistance));
+        Vector2 currentPosition = transform.position;
+        wayPoint = new Vector2(
+            currentPosition.x + Random.Range(-maxDistance, maxDistance),
+            currentPosition.y + Random.Range(-maxDistance, maxDistance)
+        );
     }
-
 }
