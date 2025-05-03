5d9e9693cc3867b9ded55a3107280cd916d0e291
Viros2p <arma0096x@gmail.com>
2025-05-03 18:58:18 +0200

    Lavet musik for alle scenerne
    både ond musik og normlt musik

--- Assets/_Game_/Player/Visuals/Scipts/WaypointMover.cs
+++ Assets/_Game_/Player/Visuals/Scipts/WaypointMover.cs
@@ -30,7 +30,7 @@ public class WaypointMover : MonoBehaviour
 
     void Update()
     {
-        // Hvis de ikke må bevæge os eller venter, gør vi ingenting
+        // Hvis de ikke må bevæge os eller venter, gør de ingenting
         if (!isMoving || IsWaiting)
             return;
 
